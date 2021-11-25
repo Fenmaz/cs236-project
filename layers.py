6c84e1cdfbbf5250dc542a8c4cdd208e052a535d
Trung Nguyen <trung.chi.nguyen.97@gmail.com>
2021-11-25 02:39:49 -0600

    Adapt to LMConv implementation.

--- layers.py
+++ layers.py
@@ -10,99 +10,13 @@ class nin(nn.Module):
         self.dim_out = dim_out
 
     def forward(self, x):
-        """ a network in network layer (1x1 CONV) """
+        """ A network in network layer (1x1 CONV) """
         x = x.permute(0, 2, 3, 1)
         shp = list(x.size())
         out = self.lin_a(x.contiguous().view(shp[0] * shp[1] * shp[2], shp[3]))
         shp[-1] = self.dim_out
         out = out.view(shp)
         return out.permute(0, 3, 1, 2)
-
-
-class down_shifted_conv2d(nn.Module):
-    def __init__(self, num_filters_in, num_filters_out, filter_size=(2, 3), stride=(1, 1),
-                 shift_output_down=False, norm='weight_norm'):
-        super(down_shifted_conv2d, self).__init__()
-
-        assert norm in [None, 'batch_norm', 'weight_norm']
-        self.conv = nn.Conv2d(num_filters_in, num_filters_out, filter_size, stride)
-        self.shift_output_down = shift_output_down
-        self.norm = norm
-        self.pad = nn.ZeroPad2d((int((filter_size[1] - 1) / 2),  # pad left
-                                 int((filter_size[1] - 1) / 2),  # pad right
-                                 filter_size[0] - 1,  # pad top
-                                 0))  # pad down
-
-        if norm == 'weight_norm':
-            self.conv = wn(self.conv)
-        elif norm == 'batch_norm':
-            self.bn = nn.BatchNorm2d(num_filters_out)
-
-        if shift_output_down:
-            self.down_shift = lambda x: down_shift(x, pad=nn.ZeroPad2d((0, 0, 1, 0)))
-
-    def forward(self, x):
-        x = self.pad(x)
-        x = self.conv(x)
-        x = self.bn(x) if self.norm == 'batch_norm' else x
-        return self.down_shift(x) if self.shift_output_down else x
-
-
-class down_shifted_deconv2d(nn.Module):
-    def __init__(self, num_filters_in, num_filters_out, filter_size=(2, 3), stride=(1, 1)):
-        super(down_shifted_deconv2d, self).__init__()
-        self.deconv = wn(nn.ConvTranspose2d(num_filters_in, num_filters_out, filter_size, stride,
-                                            output_padding=1))
-        self.filter_size = filter_size
-        self.stride = stride
-
-    def forward(self, x):
-        x = self.deconv(x)
-        xs = [int(y) for y in x.size()]
-        return x[:, :, :(xs[2] - self.filter_size[0] + 1),
-               int((self.filter_size[1] - 1) / 2):(xs[3] - int((self.filter_size[1] - 1) / 2))]
-
-
-class down_right_shifted_conv2d(nn.Module):
-    def __init__(self, num_filters_in, num_filters_out, filter_size=(2, 2), stride=(1, 1),
-                 shift_output_right=False, norm='weight_norm'):
-        super(down_right_shifted_conv2d, self).__init__()
-
-        assert norm in [None, 'batch_norm', 'weight_norm']
-        self.pad = nn.ZeroPad2d((filter_size[1] - 1, 0, filter_size[0] - 1, 0))
-        self.conv = nn.Conv2d(num_filters_in, num_filters_out, filter_size, stride=stride)
-        self.shift_output_right = shift_output_right
-        self.norm = norm
-
-        if norm == 'weight_norm':
-            self.conv = wn(self.conv)
-        elif norm == 'batch_norm':
-            self.bn = nn.BatchNorm2d(num_filters_out)
-
-        if shift_output_right:
-            self.right_shift = lambda x: right_shift(x, pad=nn.ZeroPad2d((1, 0, 0, 0)))
-
-    def forward(self, x):
-        x = self.pad(x)
-        x = self.conv(x)
-        x = self.bn(x) if self.norm == 'batch_norm' else x
-        return self.right_shift(x) if self.shift_output_right else x
-
-
-class down_right_shifted_deconv2d(nn.Module):
-    def __init__(self, num_filters_in, num_filters_out, filter_size=(2, 2), stride=(1, 1),
-                 shift_output_right=False):
-        super(down_right_shifted_deconv2d, self).__init__()
-        self.deconv = wn(nn.ConvTranspose2d(num_filters_in, num_filters_out, filter_size,
-                                            stride, output_padding=1))
-        self.filter_size = filter_size
-        self.stride = stride
-
-    def forward(self, x):
-        x = self.deconv(x)
-        xs = [int(y) for y in x.size()]
-        x = x[:, :, :(xs[2] - self.filter_size[0] + 1):, :(xs[3] - self.filter_size[1] + 1)]
-        return x
 
 
 '''
@@ -122,7 +36,7 @@ class GatedResnet(nn.Module):
         if skip_connection != 0:
             self.nin_skip = nin(2 * skip_connection * num_filters, num_filters)
 
-        self.dropout = nn.Dropout2d(0.5)
+        # self.dropout = nn.Dropout2d(0.5)
         self.conv_out = conv_op(2 * num_filters, 2 * num_filters)
 
     def forward(self, og_x, a=None):
@@ -130,7 +44,7 @@ class GatedResnet(nn.Module):
         if a is not None:
             x += self.nin_skip(self.nonlinearity(a))
         x = self.nonlinearity(x)
-        x = self.dropout(x)
+        # x = self.dropout(x)
         x = self.conv_out(x)
         a, b = torch.chunk(x, 2, dim=1)
         c3 = a * torch.sigmoid(b)
--- model.py
+++ model.py
@@ -1,142 +1,109 @@
+import torch
+
 from layers import *
 from utils import *
+from locally_masked_convolution import LocallyMaskedConv2d
 
 
-class PixelCNNLayer_up(nn.Module):
-    def __init__(self, nr_resnet, nr_filters, resnet_nonlinearity):
-        super(PixelCNNLayer_up, self).__init__()
+class PixelCnnLayerUp(nn.Module):
+    """
+    Sequence of (downsampling) convolution layers.
+    PixelCNN/PixelCNN++ uses downsampling layers in 2 streams (down and down-right).
+    LM PixelCNN uses dilated convolutions.
+    """
+
+    def __init__(self, nr_resnet, nr_filters, resnet_nonlinearity, conv_op):
+        super(PixelCnnLayerUp, self).__init__()
         self.nr_resnet = nr_resnet
-        # stream from pixels above
-        self.u_stream = nn.ModuleList([GatedResnet(nr_filters, down_shifted_conv2d,
+        self.u_stream = nn.ModuleList([GatedResnet(nr_filters, conv_op,
                                                    resnet_nonlinearity, skip_connection=0)
                                        for _ in range(nr_resnet)])
 
-        # stream from pixels above and to the left
-        self.ul_stream = nn.ModuleList([GatedResnet(nr_filters, down_right_shifted_conv2d,
-                                                    resnet_nonlinearity, skip_connection=1)
-                                        for _ in range(nr_resnet)])
-
-    def forward(self, u, ul):
-        u_list, ul_list = [], []
-
-        for i in range(self.nr_resnet):
-            u = self.u_stream[i](u)
-            ul = self.ul_stream[i](ul, a=u)
-            u_list += [u]
-            ul_list += [ul]
-
-        return u_list, ul_list
+    def forward(self, u, mask=None):
+        u_list = [layer(u, mask=mask) for layer in self.u_stream]
+        return u_list
 
 
-class PixelCNNLayer_down(nn.Module):
-    def __init__(self, nr_resnet, nr_filters, resnet_nonlinearity):
-        super(PixelCNNLayer_down, self).__init__()
-        self.nr_resnet = nr_resnet
-        # stream from pixels above
-        self.u_stream = nn.ModuleList([GatedResnet(nr_filters, down_shifted_conv2d,
+class PixelCnnLayerDown(nn.Module):
+    """
+    Sequence of convolution layers in the second half of the U-net, with
+    residual connections to blocks in the first half.
+    """
+
+    def __init__(self, nr_resnet, nr_filters, resnet_nonlinearity, conv_op):
+        super(PixelCnnLayerDown, self).__init__()
+        self.u_stream = nn.ModuleList([GatedResnet(nr_filters, conv_op,
                                                    resnet_nonlinearity, skip_connection=1)
                                        for _ in range(nr_resnet)])
 
-        # stream from pixels above and to the left
-        self.ul_stream = nn.ModuleList([GatedResnet(nr_filters, down_right_shifted_conv2d,
-                                                    resnet_nonlinearity, skip_connection=2)
-                                        for _ in range(nr_resnet)])
+    def forward(self, u, u_list, mask=None):
+        for layer in self.u_stream:
+            u = layer(u, a=u_list.pop(), mask=mask)
 
-    def forward(self, u, ul, u_list, ul_list):
-        for i in range(self.nr_resnet):
-            u = self.u_stream[i](u, a=u_list.pop())
-            ul = self.ul_stream[i](ul, a=torch.cat((u, ul_list.pop()), 1))
-
-        return u, ul
+        return u
 
 
 class PixelCNN(nn.Module):
     def __init__(self, nr_resnet=5, nr_filters=80, nr_logistic_mix=10, input_channels=3):
         super(PixelCNN, self).__init__()
-        self.resnet_nonlinearity = concat_elu
-        self.nr_filters = nr_filters
-        self.input_channels = input_channels
-        self.nr_logistic_mix = nr_logistic_mix
-        self.right_shift_pad = nn.ZeroPad2d((1, 0, 0, 0))
-        self.down_shift_pad = nn.ZeroPad2d((0, 0, 1, 0))
+        resnet_nonlinearity = concat_elu
+        kernel_size = (5, 5)
+        max_dilation = 2
+
+        def conv_op_init(cin, cout):
+            return wn(LocallyMaskedConv2d(cin, cout, kernel_size=kernel_size))
+
+        def conv_op_dilated(cin, cout):
+            return wn(LocallyMaskedConv2d(cin, cout, kernel_size=kernel_size, dilation=max_dilation))
+
+        def conv_op(cin, cout):
+            return wn(LocallyMaskedConv2d(cin, cout, kernel_size=kernel_size))
 
         down_nr_resnet = [nr_resnet] + [nr_resnet + 1] * 2
-        self.down_layers = nn.ModuleList([PixelCNNLayer_down(down_nr_resnet[i], nr_filters,
-                                                             self.resnet_nonlinearity) for i in range(3)])
+        self.down_layers = nn.ModuleList([PixelCnnLayerDown(down_nr_resnet[i], nr_filters,
+                                                            resnet_nonlinearity, conv_op) for i in range(3)])
 
-        self.up_layers = nn.ModuleList([PixelCNNLayer_up(nr_resnet, nr_filters,
-                                                         self.resnet_nonlinearity) for _ in range(3)])
+        self.up_layers = nn.ModuleList([PixelCnnLayerUp(nr_resnet, nr_filters,
+                                                        resnet_nonlinearity, conv_op) for _ in range(3)])
 
-        self.downsize_u_stream = nn.ModuleList([down_shifted_conv2d(nr_filters, nr_filters,
-                                                                    stride=(2, 2)) for _ in range(2)])
+        self.downsize_u_stream = nn.ModuleList([conv_op_dilated(nr_filters, nr_filters) for _ in range(2)])
+        self.upsize_u_stream = nn.ModuleList([conv_op_dilated(nr_filters, nr_filters) for _ in range(2)])
 
-        self.downsize_ul_stream = nn.ModuleList([down_right_shifted_conv2d(nr_filters,
-                                                                           nr_filters, stride=(2, 2)) for _ in
-                                                 range(2)])
+        self.u_init = conv_op_init(input_channels + 1, nr_filters)
 
-        self.upsize_u_stream = nn.ModuleList([down_shifted_deconv2d(nr_filters, nr_filters,
-                                                                    stride=(2, 2)) for _ in range(2)])
-
-        self.upsize_ul_stream = nn.ModuleList([down_right_shifted_deconv2d(nr_filters,
-                                                                           nr_filters, stride=(2, 2)) for _ in
-                                               range(2)])
-
-        self.u_init = down_shifted_conv2d(input_channels + 1, nr_filters, filter_size=(2, 3),
-                                          shift_output_down=True)
-
-        self.ul_init = nn.ModuleList([down_shifted_conv2d(input_channels + 1, nr_filters,
-                                                          filter_size=(1, 3), shift_output_down=True),
-                                      down_right_shifted_conv2d(input_channels + 1, nr_filters,
-                                                                filter_size=(2, 1), shift_output_right=True)])
-
-        num_mix = 3 if self.input_channels == 1 else 10
+        num_mix = 3 if input_channels == 1 else 10
         self.nin_out = nin(nr_filters, num_mix * nr_logistic_mix)
         self.init_padding = None
 
-    def forward(self, x, sample=False):
-        # similar as done in the tf repo :
+    def forward(self, x, mask_init, mask_undilated, mask_dilated, sample=False):
         if self.init_padding is None and not sample:
             padding_s = list(x.size())
             padding_s[1] = 1
-            padding = torch.ones(padding_s, requires_grad=False)
-            self.init_padding = padding.cuda() if x.is_cuda else padding
+            self.init_padding = torch.ones(padding_s, requires_grad=False, device=x.device)
 
         if sample:
             padding_s = list(x.size())
             padding_s[1] = 1
-            padding = torch.ones(padding_s, requires_grad=False)
-            padding = padding.cuda() if x.is_cuda else padding
+            padding = torch.ones(padding_s, requires_grad=False, device=x.device)
             x = torch.cat((x, padding), 1)
 
-        # Upstream
         x = x if sample else torch.cat((x, self.init_padding), 1)
-        u_list = [self.u_init(x)]
-        ul_list = [self.ul_init[0](x) + self.ul_init[1](x)]
-        for i in range(3):
-            # resnet block
-            u_out, ul_out = self.up_layers[i](u_list[-1], ul_list[-1])
-            u_list += u_out
-            ul_list += ul_out
 
-            if i != 2:
-                # downscale (only twice)
-                u_list += [self.downsize_u_stream[i](u_list[-1])]
-                ul_list += [self.downsize_ul_stream[i](ul_list[-1])]
+        # Up pass
+        u_list = [self.u_init(x, mask=mask_init)]
+        for i in range(2):
+            u_list.append(self.up_layers[i](u_list[-1], mask=mask_undilated))
+            u_list.append(self.downsize_u_stream[i](u_list[-1], mask=mask_dilated))
+        u_list.append(self.up_layers[2](u_list[-1], mask=mask_undilated))
 
-        # Downstream
+        # Down pass
         u = u_list.pop()
-        ul = ul_list.pop()
+        for i in range(2):
+            u = self.down_layers[i](u, u_list, mask=mask_undilated)
+            u = self.upsize_u_stream[i](u, mask=mask_dilated)
+        u = self.down_layers[2](u, u_list, mask=mask_undilated)
 
-        for i in range(3):
-            # resnet block
-            u, ul = self.down_layers[i](u, ul, u_list, ul_list)
-
-            # upscale (only twice)
-            if i != 2:
-                u = self.upsize_u_stream[i](u)
-                ul = self.upsize_ul_stream[i](ul)
-
-        x_out = self.nin_out(F.elu(ul))
+        x_out = self.nin_out(F.elu(u))
         return x_out
 
 
@@ -151,13 +118,10 @@ if __name__ == '__main__':
     loss = discretized_mix_logistic_loss(y_t, x_t)
     print('loss : %s' % loss.data.item())
 
-    ''' testing model and deconv dimensions '''
-    x = torch.FloatTensor(32, 3, 32, 32).uniform_(-1., 1.).to(device)
-    ds = down_shifted_deconv2d(3, 40, stride=(2, 2))
-
     ''' testing loss compatibility '''
+    x = torch.rand((32, 3, 32, 32), device=device) * 2 - 1
     model = PixelCNN(nr_resnet=3, nr_filters=100, input_channels=x.size(1))
     model = model.to(device)
-    out = model(x)
+    out = model(x, None, None, None)
     loss = discretized_mix_logistic_loss(x, out)
     print('loss : %s' % loss.data.item())
